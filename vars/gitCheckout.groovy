a854dee9b71d905629a2801f5c11d7342005d0d4
Sapkota Sushma <a01363028@unet.univie.ac.at>
2024-02-09 11:00:29 +0100

    gitCheckout added

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,8 @@
 def call(Map stageParams) {
+ 
     checkout([
         $class: 'GitSCM',
-        branches: [[name: stageParams.branch]],
-        userRemoteConfigs: [[url: stageParams.url]]
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-}
+  }
